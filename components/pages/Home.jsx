55b59fd409a67f9c7011d2422ab9e86221e5a9d3
MuftiBinHabib <muftibinhabib500@gmail.com>
2025-04-25 19:33:32 +0600

    fixed routing

--- components/pages/Home.jsx
+++ components/pages/Home.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Suppliers from '../Suppliers'
 import HowItWorks from '../HowItWorks'
 import Features from '../Features'
+import Header from '../Header'
+import Banner from '../Banner'
 
 const Home = () => {
   return (
     <div>
+      
+      <Banner />
         <Features />
       <HowItWorks />
-      <Suppliers />
+      
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 import RootLayout from './layout/RootLayout.jsx';
 import Suppliers from '../components/Suppliers.jsx';
-
+import Home from '../components/pages/Home.jsx';
 
 const router = createBrowserRouter([
   {
-    path : "/",
-    Component : RootLayout,
-    
-    
-
-    children : [
-     
+    path: '/',
+    element: <RootLayout />,
+    children: [
       {
-
-        path : "/items",
-      element : <Suppliers />
-
-      }
-      
-    ]
-  }
-])
+        index: true, // This makes Home the default route
+        element: <Home />,
+      },
+      {
+        path: 'items',
+        element: <Suppliers />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
-)
+);
